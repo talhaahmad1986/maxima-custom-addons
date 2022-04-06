4c9fd1ebca7fb8ce4902c2bd2bc8db117ba7381a
mjawad <jawkhan111@gmail.com>
2022-04-07 00:30:17 +0500

    HS code and Access rights and unit price do to inv  task done

--- eq_invoice_from_picking/models/stock.py
+++ eq_invoice_from_picking/models/stock.py
@@ -9,7 +9,8 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 from datetime import datetime
 
-from odoo.odoo.exceptions import UserError
+from odoo.exceptions import UserError
+from odoo.tools import float_compare, float_is_zero
 
 journal_type_dict = {
             ('outgoing', 'customer'): ['out_invoice'],
@@ -65,7 +66,115 @@ class stock_picking(models.Model):
         }
         return invoice_vals
 
+    def button_validate(self):
+        # Clean-up the context key at validation to avoid forcing the creation of immediate
+        # transfers.
+        gr = self.write_uid.groups_id.filtered(lambda a: a.name == 'Move Confirmation')
+        if gr:
+            ctx = dict(self.env.context)
+            ctx.pop('default_immediate_transfer', None)
+            self = self.with_context(ctx)
 
+            # Sanity checks.
+            pickings_without_moves = self.browse()
+            pickings_without_quantities = self.browse()
+            pickings_without_lots = self.browse()
+            products_without_lots = self.env['product.product']
+            for picking in self:
+                if not picking.move_lines and not picking.move_line_ids:
+                    pickings_without_moves |= picking
+
+                picking.message_subscribe([self.env.user.partner_id.id])
+                picking_type = picking.picking_type_id
+                precision_digits = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+                no_quantities_done = all(
+                    float_is_zero(move_line.qty_done, precision_digits=precision_digits) for move_line in
+                    picking.move_line_ids.filtered(lambda m: m.state not in ('done', 'cancel')))
+                no_reserved_quantities = all(
+                    float_is_zero(move_line.product_qty, precision_rounding=move_line.product_uom_id.rounding) for
+                    move_line in picking.move_line_ids)
+                if no_reserved_quantities and no_quantities_done:
+                    pickings_without_quantities |= picking
+
+                if picking_type.use_create_lots or picking_type.use_existing_lots:
+                    lines_to_check = picking.move_line_ids
+                    if not no_quantities_done:
+                        lines_to_check = lines_to_check.filtered(lambda line: float_compare(line.qty_done, 0,
+                                                                                            precision_rounding=line.product_uom_id.rounding))
+                    for line in lines_to_check:
+                        product = line.product_id
+                        if product and product.tracking != 'none':
+                            if not line.lot_name and not line.lot_id:
+                                pickings_without_lots |= picking
+                                products_without_lots |= product
+
+            if not self._should_show_transfers():
+                if pickings_without_moves:
+                    raise UserError(_('Please add some items to move.'))
+                if pickings_without_quantities:
+                    raise UserError(self._get_without_quantities_error_message())
+                if pickings_without_lots:
+                    raise UserError(_('You need to supply a Lot/Serial number for products %s.') % ', '.join(
+                        products_without_lots.mapped('display_name')))
+            else:
+                message = ""
+                if pickings_without_moves:
+                    message += _('Transfers %s: Please add some items to move.') % ', '.join(
+                        pickings_without_moves.mapped('name'))
+                if pickings_without_quantities:
+                    message += _(
+                        '\n\nTransfers %s: You cannot validate these transfers if no quantities are reserved nor done. To force these transfers, switch in edit more and encode the done quantities.') % ', '.join(
+                        pickings_without_quantities.mapped('name'))
+                if pickings_without_lots:
+                    message += _('\n\nTransfers %s: You need to supply a Lot/Serial number for products %s.') % (
+                    ', '.join(pickings_without_lots.mapped('name')),
+                    ', '.join(products_without_lots.mapped('display_name')))
+                if message:
+                    raise UserError(message.lstrip())
+
+            # Run the pre-validation wizards. Processing a pre-validation wizard should work on the
+            # moves and/or the context and never call `_action_done`.
+            if not self.env.context.get('button_validate_picking_ids'):
+                self = self.with_context(button_validate_picking_ids=self.ids)
+            res = self._pre_action_done_hook()
+            if res is not True:
+                return res
+
+            # Call `_action_done`.
+            if self.env.context.get('picking_ids_not_to_backorder'):
+                pickings_not_to_backorder = self.browse(self.env.context['picking_ids_not_to_backorder'])
+                pickings_to_backorder = self - pickings_not_to_backorder
+            else:
+                pickings_not_to_backorder = self.env['stock.picking']
+                pickings_to_backorder = self
+            pickings_not_to_backorder.with_context(cancel_backorder=True)._action_done()
+            pickings_to_backorder.with_context(cancel_backorder=False)._action_done()
+
+            if self.user_has_groups('stock.group_reception_report') \
+                    and self.user_has_groups('stock.group_auto_reception_report') \
+                    and self.filtered(lambda p: p.picking_type_id.code != 'outgoing'):
+                lines = self.move_lines.filtered(lambda
+                                                     m: m.product_id.type == 'product' and m.state != 'cancel' and m.quantity_done and not m.move_dest_ids)
+                if lines:
+                    # don't show reception report if all already assigned/nothing to assign
+                    wh_location_ids = self.env['stock.location'].search(
+                        [('id', 'child_of', self.picking_type_id.warehouse_id.view_location_id.id),
+                         ('location_id.usage', '!=', 'supplier')]).ids
+                    if self.env['stock.move'].search([
+                        ('state', 'in', ['confirmed', 'partially_available', 'waiting', 'assigned']),
+                        ('product_qty', '>', 0),
+                        ('location_id', 'in', wh_location_ids),
+                        ('move_orig_ids', '=', False),
+                        ('picking_id', 'not in', self.ids),
+                        ('product_id', 'in', lines.product_id.ids)], limit=1):
+                        action = self.action_view_reception_report()
+                        action['context'] = {'default_picking_ids': self.ids}
+                        return action
+            return True
+        else:
+            raise ValidationError(_(
+                "You are not allow to perform this operation. "
+            ))
 class account_move(models.Model):
     _inherit = 'account.move'
 
@@ -234,15 +343,42 @@ class wizard_stock_picking_invoice(models.TransientModel):
         invoice_id.del_add = picking_ids.del_add
         invoice_id.mark = picking_ids.mark
         invoice_id.do_ref = picking_ids.name
-        print()
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
+    @api.depends('move_id.move_type', 'tax_ids', 'tax_repartition_line_id', 'debit', 'credit', 'tax_tag_ids')
+    def _compute_tax_tag_invert(self):
+        for record in self:
+            if not record.tax_repartition_line_id and not record.tax_ids:
+                # Invoices imported from other softwares might only have kept the tags, not the taxes.
+                record.tax_tag_invert = record.tax_tag_ids and record.move_id.is_inbound()
+
+            elif record.move_id.move_type == 'entry':
+                # For misc operations, cash basis entries and write-offs from the bank reconciliation widget
+                rep_line = record.tax_repartition_line_id
+                if rep_line:
+                    tax_type = (rep_line.refund_tax_id or rep_line.invoice_tax_id).type_tax_use
+                    is_refund = bool(rep_line.refund_tax_id)
+                elif record.tax_ids:
+                    tax_type = record.tax_ids[0].type_tax_use
+                    is_refund = (tax_type == 'sale' and record.debit) or (tax_type == 'purchase' and record.credit)
+
+                record.tax_tag_invert = (tax_type == 'purchase' and is_refund) or (tax_type == 'sale' and not is_refund)
+
+            else:
+                # For invoices with taxes
+                record.tax_tag_invert = record.move_id.is_inbound()
+        if self.env.context.get('active_model') == 'stock.picking':
+            picking_id = self.env['stock.picking'].browse(self.env.context.get('active_id'))
+            for record in self:
+                for move in picking_id.move_line_ids:
+                    record.price = move.move_id.price
+                    print()
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    wc = fields.Char('WC')
+    # wc = fields.Char('WC')
     deadline = fields.Char('Deadline')
     desc = fields.Char('Description')
     packing = fields.Many2one('product.packaging','Packaging')
@@ -257,6 +393,18 @@ class AccountMove(models.Model):
     mark = fields.Char('Mark')
     do_ref = fields.Char('DO Reference')
 
+    def action_post(self):
+        gr = self.write_uid.groups_id.filtered(lambda a: a.name == 'Move Confirmation')
+        if gr:
+            if self.payment_id:
+                self.payment_id.action_post()
+            else:
+                self._post(soft=False)
+            return False
+        else:
+            raise ValidationError(_(
+                "You are not allow to perform this operation. "
+            ))
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
@@ -272,3 +420,49 @@ class AccountMoveLine(models.Model):
         else:
             for rec in self:
                 rec.price = rec.price
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    hs_code = fields.Char('HS Code')
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        ''' Store the initial standard price in order to be able to retrieve the cost of a product template for a given date'''
+        is_right = True
+        if self.env.context.get('uid'):
+            users = self.env['res.users'].search([('id', '=', self.env.context.get('uid'))])
+            gr = users.groups_id.filtered(lambda a: a.name == 'Product Creation')
+            if gr:
+                for vals in vals_list:
+                    self._sanitize_vals(vals)
+                templates = super(ProductTemplate, self).create(vals_list)
+                if "create_product_product" not in self._context:
+                    templates._create_variant_ids()
+
+                # This is needed to set given values to first variant after creation
+                for template, vals in zip(templates, vals_list):
+                    related_vals = {}
+                    if vals.get('barcode'):
+                        related_vals['barcode'] = vals['barcode']
+                    if vals.get('default_code'):
+                        related_vals['default_code'] = vals['default_code']
+                    if vals.get('standard_price'):
+                        related_vals['standard_price'] = vals['standard_price']
+                    if vals.get('volume'):
+                        related_vals['volume'] = vals['volume']
+                    if vals.get('weight'):
+                        related_vals['weight'] = vals['weight']
+                    # Please do forward port
+                    if vals.get('packaging_ids'):
+                        related_vals['packaging_ids'] = vals['packaging_ids']
+                    if related_vals:
+                        template.write(related_vals)
+            else:
+                is_right = False
+
+        if not is_right:
+            raise ValidationError(_(
+                "You are not allow to perform this operation. "
+            ))
+        return templates
